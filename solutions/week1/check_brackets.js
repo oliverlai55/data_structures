76662e630f699a57a1debac735e20b70ae04e78d
oliverlai55 <oliverlai55@gmail.com>
2018-04-17 02:45:19 -0400

    Add progress

--- solutions/week1/check_brackets.js
+++ solutions/week1/check_brackets.js
@@ -2,30 +2,56 @@ const readline = require('readline');
 process.stdin.setEncoding('utf8');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-})
+	input: process.stdin,
+});
 
 const readLine = line => {
-  console.log(checkBrackets(line));
-  process.exit();
-}
+	console.log(checkBrackets(line));
+	process.exit();
+};
 
 rl.on('line', readLine);
 
-const checkBrackets = (str) => {
-  let stack = [];
-  for (let char of str ) {
-    if (char === '(' || '[') {
-      stack.push(char);
-    } else {
-      // check if stack is empty
-      if (stack.length === 0) return false;
-      let top = stack.pop();
-      if ((top === '[' && char != ']') ||
-        (top === '(' && char != ')')) {
-          return false;
-        }
-    }
-  }
-  return stack.length === 0;
-}
+const checkBrackets = str => {
+	let indexOfStr;
+	let stack = [];
+	// for (let char of str) {
+	// 	if (char === '(' || '[' || '{') {
+	// 		stack.push(char);
+	// 	} else {
+	// 		// check if stack is empty
+	// 		console.log(stack.length);
+	//
+	// 		let top = stack.pop();
+	// 		console.log('top', top);
+	// 		if ((top === '[' && char != ']') || (top === '(' && char != ')')) {
+	// 			return false;
+	// 		}
+	// 	}
+	// }
+
+	for (let index in str) {
+		let char = str[index];
+
+		if (char === '(' || '[' || '{') {
+			// console.log('char', char);
+			stack.push(char);
+			console.log(stack);
+		} else {
+			let top = stack.pop();
+			if (
+				(top === '[' && char !== ']') ||
+				(top === '(' && char !== ')') ||
+				(top === '{' && char !== '}')
+			) {
+				indexOfStr = index * 1 + 1;
+				return indexOfStr;
+			} else {
+				stack.pop();
+			}
+		}
+	}
+	let result = stack.length ? indexOfStr : 'Success';
+	// console.log(result);
+	return result;
+};
