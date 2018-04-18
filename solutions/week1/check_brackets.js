0344d1a9909ce5ff7c78be6fbc43e40bfaab3fe8
oliverlai55 <oliverlai55@gmail.com>
2018-04-18 00:48:25 -0400

    Make changes

--- solutions/week1/check_brackets.js
+++ solutions/week1/check_brackets.js
@@ -12,28 +12,33 @@ const readLine = line => {
 
 rl.on('line', readLine);
 
-const checkBrackets = (str) => {
-  if (str.length == 0) return false;
+const checkBrackets = str => {
+	if (str.length == 0) return false;
 
-  let matchingOpenBracket, char, index, result
-  let stack = [];
+	let matchingOpenBracket, matchingClosedBracket, char, index, result;
+	let stack = [];
 
-  let openingBrackets = ['[', '(', '{'];
-  let closingBrackets = [']', ')', '}'];
+	let openingBrackets = ['[', '(', '{'];
+	let closingBrackets = [']', ')', '}'];
 
-  for (index = 0; index < str.length; index++) {
-    char = str[index];
+	for (index = 0; index < str.length; index++) {
+		char = str[index];
 
-    if (closingBrackets.indexOf(char) > -1) {
-      matchingOpenBracket = openingBrackets[closingBrackets.indexOf(char)]
-      if (stack.length == 0 || (stack.pop() != matchingOpenBracket)) {
-        index = index * 1 + 1;
-        return index;
-      }
-    } else {
-      stack.push(char);
-    }
-  }
-  result = stack.length == 0 ? 'Success' : index;
-  return result;
-}
+		// if (closingBrackets.indexOf(char) > -1) {
+		// 	let topBracket = stack.pop();
+		// 	matchingOpenBracket = openingBrackets[closingBrackets.indexOf(char)];
+		// 	matchingClosedBracket = closingBrackets[openingBrackets.indexOf(char)];
+		//
+		// 	if (stack.length == 0 || stack.pop() != matchingOpenBracket) {
+		// 		index = index * 1 + 1;
+		// 		return index;
+		// 	}
+		//
+		//
+		// } else {
+		// 	stack.push(char);
+		// }
+	}
+	result = stack.length == 0 ? 'Success' : index;
+	return result;
+};
